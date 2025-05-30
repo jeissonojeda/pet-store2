cdecfb0871fb51abdb6849869a4e946afc1b4079
jeisson <ojedajeisson7@gmail.com>
2025-05-29 21:55:29 -0500

    integrar bustrap

---
+++ src/config/database.php
@@ -0,0 +1,37 @@
+<?php   
+/*Supabase db connecttion
+$host = 'aws-0-us-east-1.pooler.supabase.com';
+$port = '5432';
+$dbname = 'petstore';
+$username = 'postgres.ihxbvczkmuxyafflyeku';
+$password = 'unicesmag@@';
+*/
+ //Settings database
+$host = 'localhost';
+$port = '5432';
+$dbname = 'petstore';
+$username = 'postgres';
+$password = 'unicesmag';
+
+// Connection string
+// Connection string
+$data_connection = "
+    host=$host
+    port=$port
+    dbname=$dbname
+    user=$username
+    password=$password
+";
+
+$conn = pg_connect($data_connection);
+
+if (!$conn) {
+    echo "Connection failed: " . preg_last_error($conn);
+
+}
+
+else{
+    echo "succes!!";
+}
+
+?>
